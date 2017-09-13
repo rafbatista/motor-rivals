0cc60aa490cfbdb32b2cab0a5517c42f69414aa1
rafajr91 <rah5024@gmail.com>
2017-09-13 10:11:15 -0700

    Added images and created data model for front page of site. #1

---
+++ main.js
@@ -0,0 +1,44 @@
+var cars = [
+  {
+    year: 2018,
+    make: 'Audi',
+    model: 'A5',
+    imgSrc: /images/audi.a5.2018.jpg
+  }
+  {
+    year: 2018,
+    make: 'BMW',
+    model: '430i',
+    imgSrc: /images/bmw.430i.2018.jpg
+  }
+  {
+    year: 2018,
+    make: 'Cadillac',
+    model: 'ATS Coupe',
+    imgSrc: /images/cadillac.ats.coupe.2018.jpg
+  }
+  {
+    year: 2017,
+    make: 'Infiniti',
+    model: 'Q60',
+    imgSrc: /images/infiniti.q60.2017.jpg
+  }
+  {
+    year: 2017,
+    make: 'Lexus',
+    model: 'RC 200t',
+    imgSrc: /images/lexus.rc200t.2017.jpg
+  }
+  {
+    year: 2018,
+    make: 'Mercedez-Benz',
+    model: 'C300 Coupe',
+    imgSrc: /images/mercedez-benz.c300.coupe.2018.jpg
+  }
+]
+
+function renderCars(car) {
+    var $car = doucment.createElement('div')
+    var $img = document.createElement('img')
+
+}
