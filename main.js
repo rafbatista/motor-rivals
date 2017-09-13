6d1177fd2956f689fb02b76d24016970efc1fc36
rafajr91 <rah5024@gmail.com>
2017-09-13 15:32:52 -0700

    Defined a function in main.js that renders car title and img. #1

--- main.js
+++ main.js
@@ -38,3 +38,33 @@ var cars = [
     imgSrc: 'images/mercedez-benz-c300-coupe-2018.jpg'
   }
 ]
+
+function renderCar(car) {
+  /*
+  <div class="col s3">
+    <a class="icon-title" href="2018 Audi A5">2018 Audi A5</a>
+    <img class="car-icon" src="images/audi-a5-2018.jpg" alt="2018 Audi A5"/>
+  </div> */
+
+  // This creates the div container for car info //
+  var $car = document.createElement('div')
+  $car.setAttribute('class', 'col s3')
+
+  // This creates the link/title for the car. //
+  var $carlink = document.createElement('a')
+  $carlink.setAttribute('class', 'icon-title')
+  $carlink.setAttribute('href', '')
+  $carlink.textContent = car.year + ' ' + car.make + ' ' + car.model
+
+  // This creates the image of the car below title //
+  var $carimg = document.createElement('img')
+  $carimg.setAttribute('class', 'car-icon')
+  $carimg.setAttribute('src', car.imgSrc)
+  $carimg.setAttribute('alt', car.year + ' ' + car.make + ' ' + car.model)
+
+  /* This should complete the function by adding all the above elements to create
+  above example in the DOM */
+  $car.appendChild($carlink)
+  $car.appendChild($carimg)
+  return $car
+}
